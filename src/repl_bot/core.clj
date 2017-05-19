19b6490139a2d87590a3f33a2f29d788bf284281
Hagmonk <hagmonk@icloud.com>
2017-05-19 15:28:37 -0700

    Fixes. It works now?

--- src/repl_bot/core.clj
+++ src/repl_bot/core.clj
@@ -6,27 +6,27 @@
 
 (defonce repl-agent (agent (let [{:keys [nrepl-host nrepl-port]
                                   :or   {nrepl-host "localhost" nrepl-port 9876}} env]
-                             (repl/connect :host nrepl-host :port nrepl-port))))
+                             (-> (repl/connect :host nrepl-host :port nrepl-port)
+                                 (repl/client 5000)))))
 
 (defn log-event [type data]
   (println "\nReceived: " type " -> " data))
 
 (defn repl-send-and-receive! [message]
-  (send-off repl-agent #(-> %
-                            (repl/client 5000)
-                            (repl/message {:op "eval" :code message})))
-  ; probably racy ...
   (if (await-for 5000 repl-agent)
     (-> @repl-agent
+        (repl/message {:op "eval" :code message})
         repl/response-values
         pr-str)
     "Message timed out"))
 
 (defn repl-command [type data]
   (let [command (get data "content")
-        channel (get data "channel_id")]
+        channel (get data "channel_id")
+        user (get-in data ["author" "username"])]
     (when (and (= (some-> channel str Long/parseLong) 315213511120912386)
-               (not (re-find #"^\s*=>" command)))
+               (not (re-find #"^\s*=>" command))
+               (not (= user "repl-bot")))
       (discord/answer data (repl-send-and-receive! command)))))
 
 ; channel-id 315213511120912386
